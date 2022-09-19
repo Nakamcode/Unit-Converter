2e9e974611b5c9efbc36d2d210c32f68acee58a3
Nakam <nanaasamoahkwaw963@gmail.com>
2022-09-19 08:43:19 +0000

    enable prettier extension

--- Scripts.js
+++ Scripts.js
@@ -1,25 +1,20 @@
 var pixel = document.getElementById("px-input");
 var em = document.getElementById("em-input");
-var output ;
+var output;
 var parentValue = document.getElementById("parentValue");
 em.value = pixel.value / parentValue.value;
 
-
 valueUpdate = () => {
-    pxToEm();
-    emToPx();
-}
+  pxToEm();
+  emToPx();
+};
 
 pxToEm = () => {
-    output = pixel.value / parentValue.value ;
-    em.value = parseFloat(output.toFixed(3));
-}
-
-
+  output = pixel.value / parentValue.value;
+  em.value = parseFloat(output.toFixed(3));
+};
 
 emToPx = () => {
-    output = em.value * parentValue.value;
-    pixel.value = parseFloat(output.toFixed(0));
-}
-
-
+  output = em.value * parentValue.value;
+  pixel.value = parseFloat(output.toFixed(0));
+};
